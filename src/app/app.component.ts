672cda08f06e8909abaf664c14a02535527b537d
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2024-02-28 18:34:38 -0800

    adds files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { InputBoxComponent } from './input-box/input-box.component';
 import { TodosComponent } from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, InputBoxComponent, TodosComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    InputBoxComponent,
+    TodosComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
@@ -17,9 +24,13 @@ export class AppComponent {
 
   onTodoAdded(newTodoText: string) {
     this.todos.push({
-      id: this.todos.length + 1, // Simple ID assignment, consider a more robust method
+      id: this.todos.length + 1,
       text: newTodoText,
       completed: false,
     });
+  }
+
+  deleteTodo(todoId: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== todoId);
   }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todos',
@@ -9,8 +9,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
   @Input() todo: any;
+  @Output() deleteRequest = new EventEmitter<number>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  deleteTodo() {
+    this.deleteRequest.emit(this.todo.id);
+  }
 }
